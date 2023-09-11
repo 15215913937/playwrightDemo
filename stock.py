c40a92e18edbfca1003a88f63b4a2038dbd3b2c1
sqn <sqn@qq.com>
2023-09-12 00:38:01 +0800

    优化stock逻辑

--- stock.py
+++ stock.py
@@ -6,6 +6,7 @@ from time import sleep
 from datetime import datetime
 
 import requests
+import numpy as np
 
 url = {
     'rise_fall_url': 'https://stock.xueqiu.com/v5/stock/screener/quote/list.json?page=1&size=30&order=desc&order_by=percent&exchange=CN&market=CN&type=sha',
@@ -17,8 +18,8 @@ session = requests.Session()
 session.headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.58'}
 session.get('https://xueqiu.com/')
-
-while True:
+a = 1
+while a == 1:
     r1 = session.get(url['rise_fall_url'])
     data1 = r1.json()
     s1 = data1['data']['list']
@@ -47,8 +48,13 @@ while True:
         turnover_ranking.append(s['name'])
 
     # print(data1)
-    print('------'+datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'------')
+    print('------' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '------')
     print(rise_fall_list)
     print(volume_ranking)
     print(turnover_ranking)
+    from functools import reduce
+    # 找出交集
+    result = reduce(np.intersect1d, (rise_fall_list, volume_ranking, turnover_ranking))
+    print(result)
     sleep(3)
+    a = 0
